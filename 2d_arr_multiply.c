1b5da631bd1d918f52da61d8ca8986dffb24a5a9
nischal shrestha <nischalstha9@gmail.com>
2021-03-03 09:45:38 +0545

    Arry mul still not dont

--- 2d_arr_multiply.c
+++ 2d_arr_multiply.c
@@ -1,26 +1,48 @@
 #include <stdio.h>
-#define rows 2
-#define cols 2
+#define ROWS 2
+#define COLS 2
 int main(int argc, char const *argv[])
 {
-    int array1[rows][cols] = {{1, 2}, {3, 4}}, array2[rows][cols] = {{5, 6}, {7, 8}}, i, j, mul[rows][cols], cache, index = 0;
-    for (i = 0; i < rows; i++)
+    int mat1[ROWS][COLS] = {{1, 2}, {3, 4}}, mat2[ROWS][COLS] = {{5, 6}, {7, 8}}, i = 0, j = 0, k = 0, mul[ROWS][COLS];
+    for (i = 0; i < ROWS; ++i)
     {
-        for (j = 0; j < cols; j++)
+        for (j = 0; j < COLS; ++j)
         {
-            cache += array1[i][j] * array2[j][i];
+            for (k = 0; k < COLS; ++k)
+            {
+                mul[i][j] += mat1[i][k] * mat2[k][j];
+            }
+            printf("\n%d", mul[i][j]);
         }
-        mul[i][index] = cache;
-        index = i < rows - 1 ? index++ : 0;
-        cache = 0;
     }
-    for (i = 0; i < rows; i++)
-    {
-        for (j = 0; j < cols; j++)
-        {
-            printf("%d\t", mul[i][j]);
-        }
-        printf("\n");
-    }
+
+    // for (i = 0; i < ROWS; i++)
+    // {
+    //     for (j = 0; j < COLS; j++)
+    //     {
+    //         while (k < ROWS)
+    //         {
+    //             cache += mat1[i][j] * mat2[j][i];
+    //             k++;
+    //         }
+    //         printf("\n======>>>%d\t", cache);
+    //         k = 0;
+    //     }
+    //     mul[i][index] = cache;
+    //     index++;
+    //     if (index == COLS - 1)
+    //     {
+    //         index = 0;
+    //     }
+    //     cache = 0;
+    // }
+    // for (i = 0; i < ROWS; i++)
+    // {
+    //     for (j = 0; j < COLS; j++)
+    //     {
+    //         printf("%d\t", mul[i][j]);
+    //     }
+    //     printf("\n");
+    // }
     return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,8 @@
-array2
+r (i = 0; i < rows; i++)
+    // {
+    //     for (j = 0; j < cols; j++)
+    //     {
+    //         printf("%d\t", mul[i][j]);
+    //     }
+    //     printf("\n");
+    // }
