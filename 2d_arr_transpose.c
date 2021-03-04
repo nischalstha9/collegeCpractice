5f3ddf77e8032b6ceca209b45d1773b58eb2da29
Nischal Shrestha <nischalstha9@gmail.com>
2021-03-04 15:25:59 +0545

    pulling progress from colz

--- 2d_arr_transpose.c
+++ 2d_arr_transpose.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #define ROWS 2
-#define COLS 2
+#define COLS 3
 int main(int argc, char const *argv[])
 {
-    int mat[ROWS][COLS] = {{1, 2}, {3, 4}}, i, j, cache;
+    int mat[ROWS][COLS] = {{1, 2, 3}, {4, 5, 6}}, i, j, cache, trans[COLS][ROWS];
     for (i = 0; i < ROWS; i++)
     {
-        for (j = 0; j < ROWS; j++)
+        for (j = 0; j < COLS; j++)
         {
-            /* code */
+            if ((i != 0 && j != 0) || (i != ROWS - 1 && j != COLS - 1))
+                trans[i][j] = mat[j][i];
+            else
+                trans[i][j] = mat[i][j];
         }
+    }
+    for (i = 0; i < ROWS; i++)
+    {
+        for (j = 0; j < COLS; j++)
+        {
+            printf("%d", trans[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
