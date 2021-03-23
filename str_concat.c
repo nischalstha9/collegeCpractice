3ebc8790416bb3156e7e856408deedbce95ca4b5
nischal shrestha <nischalstha9@gmail.com>
2021-03-23 10:40:14 +0545

    pointer

--- str_concat.c
+++ str_concat.c
@@ -9,6 +9,9 @@ int main(int argc, char const *argv[])
     puts("Enter string 2:");
     gets(str2);
     strcat(str, str2); //adds str and str2 and puts value on str
-    printf("%s", str);
+    printf("%s\n", str);
+    printf("%s\n", str2);
+    strcpy(str2, str);
+    printf("%s\n", str2);
     return 0;
 }
