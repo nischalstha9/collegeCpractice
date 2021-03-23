6978201a0df29741197c58a074bd674b483bfa94
Nischal Shrestha <nischalstha9@gmail.com>
2021-03-23 17:33:00 +0545

    pulling from colg

--- sum of int.c
+++ sum of int.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-void main(){
+#include <stdio.h>
+void main()
+{
 	long long int num;
 	long int sum;
 	printf("Enter integer you want sum of:\n");
 	scanf("%lld", &num);
-	while(num!=0){
-		sum += num%10;
+	while (num != 0)
+	{
+		sum += num % 10;
 		num /= 10;
 	}
 	printf("The sum is : %d\n", sum);
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,20 +1,16 @@
-int add(int a, int b)
-{
-    return a + b;
-}
-int sub(int a, int b)
-{
-    return a - b;
-}
-int mul(int a, int b)
-{
-    return a * b;
-}
-int div(int a, int b)
-{
-    return a / b;
-}
-int div(int a, int b)
-{
-    return a % b;
-}
+sub(int a, int b)
+// {
+//     return a - b;
+// }
+// int mul(int a, int b)
+// {
+//     return a * b;
+// }
+// int div(int a, int b)
+// {
+//     return a / b;
+// }
+// int rem(int a, int b)
+// {
+//     return a % b;
+// }
